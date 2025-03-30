d50f61131dab7f310be39afd132e366890b82ac8
Joonas Vali <joonas.wali@gmail.com>
2025-03-30 11:37:05 +0300

    add some new tests

--- bookreader-core/src/test/java/com/github/joonasvali/bookreaderai/textutil/TextAlignerTest.java
+++ bookreader-core/src/test/java/com/github/joonasvali/bookreaderai/textutil/TextAlignerTest.java
@@ -37,7 +37,7 @@ public class TextAlignerTest {
   @Test
   public void testMultipleIdenticalTexts() {
     String text = "The quick brown fox";
-    String[] texts = { text, text, text };
+    String[] texts = {text, text, text};
     TextAligner.AlignmentResult result = aligner.alignTexts(texts);
     assertTrue(result.isSuccess());
     assertEquals("The quick brown fox", result.getAlignedText());
@@ -51,9 +51,21 @@ public class TextAlignerTest {
     String text1 = "Thw quick brown fox\n";
     String text2 = "The quick brown fox";
     String text3 = "The quick red fox\n";
-    String[] texts = { text1, text2, text3 };
+    String[] texts = {text1, text2, text3};
     TextAligner.AlignmentResult result = aligner.alignTexts(texts);
     assertEquals("The quick brown fox\n", result.getAlignedText());
+    assertTrue(result.isSuccess());
+  }
+
+  @Test
+  public void testExtraSentences() {
+    String text1 = "How are you?\nI'm xyz. what. good.\nWhat are you doing?";
+    String text2 = "How are you?\nI'm good.\nWhat are you doing?";
+    String text3 = "How are you?\nI'm good.\nWhat are you doing?";
+
+    String[] texts = {text1, text2, text3};
+    TextAligner.AlignmentResult result = aligner.alignTexts(texts);
+    assertEquals("How are you?\nI'm good.\nWhat are you doing?", result.getAlignedText());
     assertTrue(result.isSuccess());
   }
 
@@ -63,9 +75,52 @@ public class TextAlignerTest {
     String text2 = "Hello, this is a. This is a-sample text! How are you? I'm good. How are you?";
     String text3 = "Hello, this is a sample text. This is a sample text. How are you? I'm good. How are you?";
 
-    String[] texts = { text1, text2, text3 };
+    String[] texts = {text1, text2, text3};
     TextAligner.AlignmentResult result = aligner.alignTexts(texts);
     assertEquals("Hello, this is a sample text. This is a sample text! How are you? I'm good. How are you?", result.getAlignedText());
     assertTrue(result.isSuccess());
   }
+
+
+  @Test
+  public void testExtraDotsInMiddle() {
+    String text1 = """
+        xyz.
+        
+        14. august 1941.a.
+        
+        ...
+        Mice and men. Bread and butter. Cats and dogs.
+        """;
+    String text2 = """        
+        xyz.
+        
+        14. august 1941.a.
+        
+        Mice and men.
+        Bread and butter. Cats
+        and dogs.
+        """;
+
+    String text3 = """
+        xyz.
+        
+        14. august 1941. a.
+        
+        Mice and men.
+        Bread and butter. Cats and dogs
+        """;
+
+
+    String[] texts = {text1, text2, text3};
+    TextAligner.AlignmentResult result = aligner.alignTexts(texts);
+    Assertions.assertEquals("""
+        xyz.
+        
+        14. august 1941. a.
+        
+        Mice and men. Bread and butter. Bread and butter. Cats and dogs.
+        """, result.getAlignedText());
+  }
+
 }
